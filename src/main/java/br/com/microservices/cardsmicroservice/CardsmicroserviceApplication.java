924d8c7d7b7d0f57381a0fa8fc27cf9dc7df390c
oRonold <or040918@gmail.com>
2025-10-13 17:35:26 -0300

    setup cards microservice

---
+++ src/main/java/br/com/microservices/cardsmicroservice/CardsmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.cardsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservices/cardsmicroservice/CardsmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservices.cardsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
