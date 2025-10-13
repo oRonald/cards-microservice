8a5bd6dbdbc3af899930072420d54140f4b7a79a
oRonold <or040918@gmail.com>
2025-10-13 17:48:24 -0300

    refactor application name

--- src/test/java/br/com/microservices/cards/CardsmicroserviceApplicationTests.java
+++ src/test/java/br/com/microservices/cards/CardsmicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.microservices.cardsmicroservice;
+package br.com.microservices.cards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
