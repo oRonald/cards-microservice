acfb2d136a49e0376a7e0e058f102a8dea41970e
oRonold <or040918@gmail.com>
2025-10-15 13:52:58 -0300

    implement receive card data by RabbitMQ and set into ClientCard

--- src/main/java/br/com/microservices/cards/infrastructure/mqueue/CardsPostSubscriber.java
+++ src/main/java/br/com/microservices/cards/infrastructure/mqueue/CardsPostSubscriber.java
@@ -1,15 +1,40 @@
 package br.com.microservices.cards.infrastructure.mqueue;
 
+import br.com.microservices.cards.domain.Card;
+import br.com.microservices.cards.domain.ClientCard;
+import br.com.microservices.cards.domain.RequestCardData;
+import br.com.microservices.cards.infrastructure.repository.CardRepository;
+import br.com.microservices.cards.infrastructure.repository.ClientCardRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CardsPostSubscriber {
+
+    private final CardRepository repository;
+    private final ClientCardRepository clientCardRepository;
 
     @RabbitListener(queues = "${mq.queue.cards-post}")
     public void receiveRequestIssue(@Payload String payload){
-        System.out.println(payload);
+        try{
+            var mapper = new ObjectMapper();
+            RequestCardData data = mapper.readValue(payload, RequestCardData.class);
+            Card card = repository.findById(data.getCardId()).orElseThrow();
+            ClientCard clientCard = new ClientCard();
+            clientCard.setCard(card);
+            clientCard.setCpf(data.getCpf());
+            clientCard.setCardLimit(data.getCardLimit());
+            System.out.println(data.getCardLimit());
+
+            clientCardRepository.save(clientCard);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
